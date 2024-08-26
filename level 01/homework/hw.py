1e25913c03906b5716bd2e002a336a39b0fc5359
ninogviniashvili <nino201224@gmail.com>
2024-08-26 23:26:17 +0400

    second commit

---
+++ level 01/homework/hw.py
@@ -0,0 +1,8 @@
+name = nino
+lastname = gviniashvili
+age = 12
+height = 1.61
+country = Georgia
+
+
+print('my name is' + name + 'my last name is' + lastname + 'i am' + age + 'years old' + ' and i am living in' + country + 'i am ' + height)
