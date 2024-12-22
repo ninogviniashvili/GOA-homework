6615bd50ca96e139f669b20af20ce770f3c1f767
ninogviniashvili <nino201224@gmail.com>
2024-12-22 15:33:13 +0400

    second commit

--- level 01/homework/hw.py
+++ level 01/homework/hw.py
@@ -5,4 +5,4 @@ height = 1.61
 country = Georgia
 
 
-print('my name is' + name + 'my last name is' + lastname + 'i am' + age + 'years old' + ' and i am living in' + country + 'i am ' + height)
+print('my name is' + name + 'my last name is' + lastname + 'i am' + age + 'years old' + ' and i live in' + country + 'i am ' + height)
